beda6912b4625a5609f3fa5076979c0163829ac7
Alexandra Baranova <siemensixone1119@gmail.com>
2025-07-30 14:49:53 +0300

    update css

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const refs = {
   openBtn: document.querySelector(".header__menu-toggle"),
   closeBtn: document.querySelector(".mobile-menu__close-btn"),
   menu: document.querySelector(".mobile-menu__wrap"),
+  backdrop: document.querySelector(".backdrop"),
 };
 
 refs.openBtn.addEventListener("click", toggleMenu);
 refs.closeBtn.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
-  refs.menu.classList.toggle("hidden");
+  refs.menu.classList.toggle("open");
+  refs.backdrop.classList.toggle("open");
 }
