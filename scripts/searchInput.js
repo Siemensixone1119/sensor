46ce4c5c41d7ec3872e7e4fde66078d85c9b40e0
Siemensixone1119 <siemensixone1119@gmail.com>
2025-08-08 11:00:13 +0300

    remove media queries

--- scripts/searchInput.js
+++ scripts/searchInput.js
@@ -5,12 +5,14 @@ export function setupSearchToggle() {
     refs.search.classList.remove("no-transition");
     refs.search.classList.remove("search--closing");
     refs.search.classList.add("search--open");
+    document.body.classList.add("no-scroll")
     setTimeout(() =>  refs.search.querySelector("input").focus(), 200);
   });
 
   refs.backInputBtn.addEventListener("click", () => {
     refs.search.classList.remove("search--open");
     refs.search.classList.add("search--closing");
+    document.body.classList.remove("no-scroll")
     refs.search.addEventListener("transitionend", function handler(e) {
       if (e.propertyName === "transform") {
         refs.search.classList.add("no-transition");
