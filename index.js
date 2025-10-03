75ea9943e765e73c5ce34a8e4593d3800c791c2d
Siemensixone1119 <siemensixone1119@gmail.com>
2025-10-03 11:59:08 +0300

    remake char table

--- index.js
+++ index.js
@@ -23,3 +23,29 @@ certSlider();
 // hideDesk();
 hideTitle();
 // heightTable();
+
+document.addEventListener("DOMContentLoaded", () => {
+  const headerScroll = document.querySelector(".compare__row--header");
+  const bodyScroll = document.querySelector(".compare__scroll");
+
+  let isSyncingHeader = false;
+  let isSyncingBody = false;
+
+  headerScroll.addEventListener("scroll", () => {
+    if (!isSyncingHeader) {
+      isSyncingBody = true;
+      bodyScroll.scrollLeft = headerScroll.scrollLeft;
+    }
+    isSyncingHeader = false;
+  });
+
+  bodyScroll.addEventListener("scroll", () => {
+    if (!isSyncingBody) {
+      isSyncingHeader = true;
+      headerScroll.scrollLeft = bodyScroll.scrollLeft;
+    }
+    isSyncingBody = false;
+  });
+  console.log(1);
+  
+});
