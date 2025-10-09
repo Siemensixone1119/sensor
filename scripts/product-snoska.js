065c530ee67e2da79238d35468bc4804b7c07f5d
Siemensixone1119 <siemensixone1119@gmail.com>
2025-10-09 08:59:32 +0300

    make snoska

--- scripts/product-snoska.js
+++ scripts/product-snoska.js
@@ -1,27 +1,35 @@
 export function toggleSnoska() {
   const productDesc = document.querySelector(".product__backdrop");
-  const openDesc = document.querySelector(".compare__cell button");
-  const productCloseBtn = productDesc.querySelector(".product__close-btn");
+  const openDesc = document.querySelectorAll(".compare__cell button");
   const desc = productDesc.querySelector(".product__quest");
+  const text = document.querySelectorAll(".compare__info-text");
+  const span = productDesc.querySelector("span");
 
   const CLS = {
     visible: "product--visible",
     noScroll: "no-scroll",
   };
 
-  function open() {
+  function open(e) {
+    const infoText = e.currentTarget
+      .closest("span")
+      .querySelector(".compare__info-text");
     productDesc.classList.add(CLS.visible);
     desc.classList.add(CLS.visible);
     document.body.classList.add(CLS.noScroll);
+    span.textContent = infoText.textContent;
   }
 
   function close() {
-    productDesc.classList.remove(CLS.visible);
     desc.classList.remove(CLS.visible);
-    document.body.classList.remove(CLS.noScroll);
+    setTimeout(() => {
+      productDesc.classList.remove(CLS.visible);
+      document.body.classList.remove(CLS.noScroll);
+      span.innerHTML = "";
+    }, 300);
   }
 
-  openDesc.addEventListener("click", open);
+  openDesc.forEach((openBtn) => openBtn.addEventListener("click", open));
 
   productDesc.addEventListener("click", (e) => {
     console.log(e.target.className);
@@ -30,4 +38,28 @@ export function toggleSnoska() {
       close();
     }
   });
+
+  desc.addEventListener("touchstart", handleTouchStart, false);
+  desc.addEventListener("touchmove", handleTouchMove, false);
+
+  let yDown = null;
+
+  function handleTouchStart(evt) {
+    const { clientY } = evt.touches[0];
+    yDown = clientY;
+  }
+
+  function handleTouchMove(evt) {
+    if (!yDown) {
+      return;
+    }
+
+    const { clientY } = evt.touches[0];
+    const yDiff = yDown - clientY;
+
+    if (yDiff < -10) {
+      close();
+    }
+    yDown = null;
+  }
 }
